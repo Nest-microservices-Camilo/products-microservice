e6d52b85dd18e65920b09a12c19a70d209bd7f0e
camilo <camilo.reyes_granada@uao.edu.co>
2024-04-21 17:41:06 -0500

    add: TCP protocol for microservices

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -63,10 +63,11 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
 
   async update(id: number, updateProductDto: UpdateProductDto) {
     await this.findOne(id);
+    const { id: __, ...data } = updateProductDto;
 
     return this.product.update({
       where: { id },
-      data: updateProductDto,
+      data: data,
     });
   }
 
